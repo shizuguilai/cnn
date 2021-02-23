4655a251519084306faea61267f51eb4280f16f5
匡杰龙 <1214825253@qq.com>
2021-02-23 22:22:46 +0800

    test

--- 卷积神经网络/study_3.py
+++ 卷积神经网络/study_3.py
@@ -11,9 +11,11 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 batch_size = 270
 transfrom = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
-dataset = torchvision.datasets.ImageFolder('../picture', transform=transfrom)
-train_loader = torch.utils.data.DataLoader(dataset = dataset, batch_size = batch_size)
+train_dataset = torchvision.datasets.ImageFolder('../picture', transform=transfrom)
+train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = batch_size)
 
+test_dataset = torchvision.dataset.ImageFolder('../test', transform=transfrom)
+test_loader = torch.utils.data.DataLoader(dataset = test_dataset, batch_size = batch_size)
 
 """
 define model
